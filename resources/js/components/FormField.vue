c05c4a45d59b9247e794be853113de04f1e260c4
anditsung <anditsung@gmail.com>
2021-01-20 16:29:59 +0700

    allow multiple parent update options when changing selection on parent

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,7 +1,7 @@
 <template>
     <default-field :field="field" :errors="errors">
         <template slot="field">
-             <select v-model="value" class="w-full form-control form-select" :disabled="disabled">
+            <select v-model="value" class="w-full form-control form-select" :disabled="disabled">
                 <option :value="null" v-if="loaded && options.length">{{ __('Choose an option') }}</option>
                 <option :value="null" v-if="loaded && options.length == 0">{{ __('No Results') }}</option>
                 <option
@@ -16,108 +16,151 @@
 </template>
 
 <script>
-import { FormField, HandlesValidationErrors } from 'laravel-nova'
+    import { FormField, HandlesValidationErrors } from 'laravel-nova'
 
-export default {
-    mixins: [FormField, HandlesValidationErrors],
+    export default {
+        mixins: [FormField, HandlesValidationErrors],
 
-    props: ['resourceName', 'resourceId', 'field'],
+        props: ['resourceName', 'resourceId', 'field'],
 
-    data() {
-        return {
-            options: [],
-            loaded: false,
-            parentValue: null
-        }
-    },
-
-    mounted() {
-        if(!this.parent_attribute) {
-            this.updateOptions();
-        }
-
-        this.watchedComponents.forEach(component => {
-
-            let attribute = 'value'
-
-            if(component.field.component === 'belongs-to-field') {
-                attribute = 'selectedResource';
+        data() {
+            return {
+                options: [],
+                loaded: false,
+                parentValue: [],
             }
+        },
 
-            component.$watch(attribute, (value) => {
+        mounted() {
 
-                this.parentValue = (value && attribute == 'selectedResource') ? value.value : value;
+            this.watchedComponents.forEach(component => {
 
-                this.updateOptions();
-            }, { immediate: true });
-        });
-    },
+                let attribute = 'value'
 
-    computed: {
-        watchedComponents() {
-            if(!this.field.parent_attribute) {
-                return [];
-            }
+                if (component.field.component === 'belongs-to-field') {
 
-            return this.$parent.$children.filter(component => {
-                return this.isWatchingComponent(component);
+                    attribute = 'selectedResource'
+
+                }
+
+                component.$watch(attribute, (value) => {
+
+                    this.parentValue[component.field.attribute] = (value && attribute == 'selectedResource') ? value.value : value
+
+                    this.updateOptions()
+
+                }, { immediate: true })
+
             })
         },
-        endpoint() {
-            return this.field.endpoint
-                .replace('{resource-name}', this.resourceName)
-                .replace('{resource-id}', this.resourceId ? this.resourceId : '')
-                .replace('{'+ this.field.parent_attribute +'}', this.parentValue ? this.parentValue : '')
-        },
-        empty() {
-            return this.loaded && this.options.length == 0;
-        },
 
-        disabled() {
-            return this.loaded == false && (this.field.parent_attribute != undefined && this.parentValue == null) || this.options.length == 0;
-        }
-    },
+        methods: {
+            setInitialValue() {
+                this.value = this.field.value || ''
+            },
 
-    methods: {
-        setInitialValue() {
-            this.value = this.field.value || ''
-        },
+            fill(formData) {
+                formData.append(this.field.attribute, this.value || '')
+            },
 
-        fill(formData) {
-            formData.append(this.field.attribute, this.value || '')
-        },
+            isWatchingComponent(component) {
+                if (Array.isArray(this.field.parent_attribute)) {
+                    return component.field !== undefined
+                        && this.field.parent_attribute.find(attribute => attribute == component.field.attribute)
+                }
 
-        updateOptions() {
-            this.options = [];
-            this.loaded = false;
+                return component.field !== undefined
+                    && component.field.attribute == this.field.parent_attribute
+            },
 
-            if(this.notWatching() || (this.parentValue != null && this.parentValue != '')) {
-                Nova.request().get(this.endpoint)
-                    .then(response => {
-                        this.loaded = true;
-                        this.options = response.data;
-                        let optionValueExists = false;
-                        this.options.forEach(option => {
-                            if(option.value == this.value) {
-                                optionValueExists = true;
-                            }
-                        })
-
-                        if(optionValueExists == false) {
-                            this.value = null;
+            shouldUpdate() {
+                if (Array.isArray(this.field.parent_attribute)) {
+                    var parentValueValid = true
+                    this.field.parent_attribute.forEach(attribute => {
+                        if (this.parentValue[attribute] == undefined
+                            || this.parentValue[attribute] == null
+                            || this.parentValue[attribute] == ''
+                        ) {
+                            parentValueValid = false
                         }
                     })
-            }
+                    if (parentValueValid) {
+                        return true
+                    }
+                }
+
+                if (this.parentValue[this.field.parent_attribute] != null && this.parentValue[this.field.parent_attribute] != '') {
+                    return true
+                }
+
+                return false
+            },
+
+            endpoint() {
+                var endpoint = this.field.endpoint
+                    .replace('{resource-name}', this.resourceName)
+                    .replace('{resource-id}', this.resourceId ? this.resourceId : '')
+
+                if (Array.isArray(this.field.parent_attribute)) {
+                    this.field.parent_attribute.forEach(attribute => {
+                        endpoint = endpoint
+                            .replace('{' + attribute + '}', this.parentValue[attribute] ? this.parentValue[attribute] : '')
+                    })
+                }
+                else {
+                    endpoint = endpoint
+                        .replace('{'+ this.field.parent_attribute +'}', this.parentValue[this.field.parent_attribute] ? this.parentValue[this.field.parent_attribute] : '')
+                }
+
+                return endpoint
+            },
+
+            async updateOptions() {
+                this.options = []
+                this.loaded = false
+
+                if (this.shouldUpdate()) {
+
+                    Nova
+                        .request()
+                        .get(this.endpoint())
+                        .then(response => {
+                            this.options = response.data
+                            this.loaded = true
+
+                            let optionValueExists = false;
+                            this.options.forEach(option => {
+                                if(option.value == this.value) {
+                                    optionValueExists = true;
+                                }
+                            })
+
+                            if(optionValueExists == false) {
+                                this.value = null;
+                            }
+                        })
+                } else {
+                    this.loaded = false
+                }
+
+            },
+
         },
 
-        notWatching() {
-            return this.field.parent_attribute == undefined;
-        },
+        computed: {
+            watchedComponents() {
+                if (!this.field.parent_attribute) {
+                    return []
+                }
 
-        isWatchingComponent(component) {
-            return component.field !== undefined
-                && component.field.attribute == this.field.parent_attribute;
-        }
-    },
-}
+                return this.$parent.$children.filter(component => {
+                    return this.isWatchingComponent(component)
+                })
+            },
+
+            disabled() {
+                return this.loaded == false && this.options.length == 0
+            },
+        },
+    }
 </script>
---
+++ resources/js/components/FormFieldOriginal.vue
@@ -0,0 +1,123 @@
+<template>
+    <default-field :field="field" :errors="errors">
+        <template slot="field">
+            <select v-model="value" class="w-full form-control form-select" :disabled="disabled">
+                <option :value="null" v-if="loaded && options.length">{{ __('Choose an option') }}</option>
+                <option :value="null" v-if="loaded && options.length == 0">{{ __('No Results') }}</option>
+                <option
+                    :key="option.value"
+                    :value="option.value"
+                    v-for="option in options">
+                    {{ option.display }}
+                </option>
+            </select>
+        </template>
+    </default-field>
+</template>
+
+<script>
+    import { FormField, HandlesValidationErrors } from 'laravel-nova'
+
+    export default {
+        mixins: [FormField, HandlesValidationErrors],
+
+        props: ['resourceName', 'resourceId', 'field'],
+
+        data() {
+            return {
+                options: [],
+                loaded: false,
+                parentValue: null
+            }
+        },
+
+        mounted() {
+            if(!this.parent_attribute) {
+                this.updateOptions();
+            }
+
+            this.watchedComponents.forEach(component => {
+
+                let attribute = 'value'
+
+                if(component.field.component === 'belongs-to-field') {
+                    attribute = 'selectedResource';
+                }
+
+                component.$watch(attribute, (value) => {
+
+                    this.parentValue = (value && attribute == 'selectedResource') ? value.value : value;
+
+                    this.updateOptions();
+                }, { immediate: true });
+            });
+        },
+
+        computed: {
+            watchedComponents() {
+                if(!this.field.parent_attribute) {
+                    return [];
+                }
+
+                return this.$parent.$children.filter(component => {
+                    return this.isWatchingComponent(component);
+                })
+            },
+            endpoint() {
+                return this.field.endpoint
+                    .replace('{resource-name}', this.resourceName)
+                    .replace('{resource-id}', this.resourceId ? this.resourceId : '')
+                    .replace('{'+ this.field.parent_attribute +'}', this.parentValue ? this.parentValue : '')
+            },
+            empty() {
+                return this.loaded && this.options.length == 0;
+            },
+
+            disabled() {
+                return this.loaded == false && (this.field.parent_attribute != undefined && this.parentValue == null) || this.options.length == 0;
+            }
+        },
+
+        methods: {
+            setInitialValue() {
+                this.value = this.field.value || ''
+            },
+
+            fill(formData) {
+                formData.append(this.field.attribute, this.value || '')
+            },
+
+            updateOptions() {
+                this.options = [];
+                this.loaded = false;
+
+                if(this.notWatching() || (this.parentValue != null && this.parentValue != '')) {
+                    Nova.request().get(this.endpoint)
+                        .then(response => {
+                            this.loaded = true;
+                            this.options = response.data;
+                            let optionValueExists = false;
+                            this.options.forEach(option => {
+                                if(option.value == this.value) {
+                                    optionValueExists = true;
+                                }
+                            })
+
+                            if(optionValueExists == false) {
+                                this.value = null;
+                            }
+                        })
+                }
+            },
+
+            notWatching() {
+                return this.field.parent_attribute == undefined;
+            },
+
+            isWatchingComponent(component) {
+                return component.field !== undefined
+                    && component.field.attribute == this.field.parent_attribute;
+            }
+        },
+    }
+</script>
